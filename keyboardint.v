019e325bf2953cf02319f381905fd4e626f06e9c
lisali42 <lisali42@hotmail.ca>
2017-10-13 10:53:13 -0700

    Update keyboardint.v

--- keyboardint.v
+++ keyboardint.v
@@ -7,10 +7,10 @@ module keyboardint(kbd, D,E,B,F,R){
 	//Then using unary reduction to see if input == specified character
 	//and update at every keyboard press
 	always_ff @(posedge kbd){
-		D = &(character_D & kdb)| &(character_d & kbd)
-		E = &(character_E & kdb)| &(character_e & kbd)
-		B = &(character_B & kdb)| &(character_b & kbd)
-		F = &(character_F & kdb)| &(character_f & kbd)
-		R = &(character_R & kdb)| &(character_r & kbd)
+		D <= &(character_D & kdb)| &(character_d & kbd)
+		E <= &(character_E & kdb)| &(character_e & kbd)
+		B <= &(character_B & kdb)| &(character_b & kbd)
+		F <= &(character_F & kdb)| &(character_f & kbd)
+		R <= &(character_R & kdb)| &(character_r & kbd)
 	}
 }
