727ffe7d9f55507e17f35fa9dcfd83766b4f7fab
fondachau <chau.fonda@gmail.com>
2017-10-20 09:20:00 -0700

    Update keyboardaddr.v

--- keyboardaddr.v
+++ keyboardaddr.v
@@ -1,13 +1,11 @@
+module keyboardaddr (input logic [31:0]  currentaddr,
+	input logic D, E, B, F, R,
+	output logic [1:0] state,
+	input logic clk,
+output logic idle,
 
-
-module keyboardaddr (currentaddr,clk, state, D, E, B, F, R,nextaddr);
-	input logic [31:0]  currentaddr;
-	input logic D, E, B, F, R;
-	output logic [1:0] state;
-	input logic clk;
-	output logic [31:0] nextaddr;
-	//output logic [1:0] nextstate;
-	
+	output logic [31:0] nextaddr
+	);
 	parameter idleFW = 2'b00;
 	parameter idleBW = 2'b01;
 	parameter FW = 2'b10;
@@ -18,11 +16,11 @@ module keyboardaddr (currentaddr,clk, state, D, E, B, F, R,nextaddr);
 	//B = backward
 	//F = forward
 	//R = reset
-	vDFF addrVDFF(clk,nextaddr,currentaddr);
+	//assign idle=state[1];
+assign idle=state[1];	
 	always_ff @ (posedge clk)
 	begin
 					case(state)
-					default: state<= idleFW;
 					idleFW:	//making it so you can only change FW/BW 
 							if(R) begin		//reset
 								state<=idleFW;
@@ -99,17 +97,10 @@ module keyboardaddr (currentaddr,clk, state, D, E, B, F, R,nextaddr);
 								state<=BW;
 								nextaddr<=currentaddr-1'b1;
 								end
+						default: state<= idleFW;
+
 				endcase
 			end
 
 endmodule
 
-module vDFF(clk, in, out);
-	input logic clk;
-	input logic [31:0] in;
-
-	output logic [31:0] out;
-
-	always_ff @(posedge clk)
-		out<=in;
-endmodule 
