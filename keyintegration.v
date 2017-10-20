25e71b0d3863197afacc3272466314927fe4b4e6
lisali42 <lisali42@hotmail.ca>
2017-10-20 00:56:09 -0700

    Update keyintegration.v

--- keyintegration.v
+++ keyintegration.v
@@ -1,27 +1,31 @@
-module keyintegration(clk,freq,variable,newfreq,keys);
+module keyintegration(clk,newfreq,keys);
 	input logic clk;
-	input logic [31:0] freq;
+	//input logic [31:0] freq;
 	input logic [2:0] keys;
 	
 	logic [31:0] variable;
 	
 	output logic [31:0] newfreq;
 
-	always_ff @(posedge clk)
-		//if( less than 50MHz)
-		if(keys[2]) begin
-			variable<=0;
-		end
-		else if(keys[1]) begin
-			variable<=variable+32'd500
-		end
-		else if(keys[0]) begin
-			variable<=variable-32'd500;
+	always_ff @(posedge clk)begin
+		if(newfreq<32'd50000000) begin
+			if(keys[2]) begin
+				variable<=0;
+			end
+			else if(keys[1]) begin
+				variable<=variable+32'd500;
+			end
+			else if(keys[0]) begin
+				variable<=variable-32'd500;
+			end
+			else begin
+				variable<=variable;
+				
+			end
 		end
 		else begin
-			variable<=variable;
-			
+			variable<=0;
 		end
 	end
-assign newfreq = freq + variable;
+assign newfreq = 32'd22000  + variable;
 endmodule
