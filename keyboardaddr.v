d07f0b2cff198649b264a949ce76d87c8d3bc84e
lisali42 <lisali42@hotmail.ca>
2017-10-19 23:19:23 -0700

    Update keyboardaddr.v

--- keyboardaddr.v
+++ keyboardaddr.v
@@ -11,64 +11,90 @@ module keyboardaddr (currentaddr,clk, state, D, E, B, F, R,nextaddr);
 	parameter FW = 2'b10;
 	parameter BW = 2'b11;
 	
+	//D = idle
+	//E = play
+	//B = backward
+	//F = forward
+	//R = reset
 	vDFF addrVDFF(clk,nextaddr,currentaddr);
 	always_ff @ (posedge clk)
 		begin
-			case(state)
-			default: state<= idleFW;
-			idleFW:	//making it so you can only change FW/BW 
-					if(R) begin
-						state<=idleFW;
-						nextaddr<=1'b0;
-						end
-					else if(E) begin
-						state<=FW;
-						nextaddr<=currentaddr+1'b1;
-						end
-					else begin
-						state<=idleFW;
-						nextaddr<=currentaddr;
-						end
-			idleBW:
-					if(R) begin
-						state<=idleBW;
-						nextaddr<=1'b0;
-						end
-					else if(E) begin
-						state<=BW;
-						nextaddr<=currentaddr+1'b1;
-						end
-					else begin
-						state<=idleBW;
-						nextaddr<=currentaddr;
-						end
+			if(currentaddr==32'b11111111111111111111111111111111) begin
+				currentaddr<=32'b0;
+				end
+			else begin
+					case(state)
+					default: state<= idleFW;
+					idleFW:	//making it so you can only change FW/BW 
+							if(R) begin		//reset
+								state<=idleFW;
+								nextaddr<=1'b0;
+								end
+							else if(B) begin //backward
+								state<=idleBW;
+								nextaddr<=currentaddr;	
+								end
+							else if(E) begin	//start
+								state<=FW;
+								nextaddr<=currentaddr+1'b1;
+								end
+							else begin		//keep idle
+								state<=idleFW;
+								nextaddr<=currentaddr;
+								end
+					idleBW:
+							if(R) begin		//reset
+								state<=idleBW;
+								nextaddr<=1'b0;
+								end
+							else if(F) begin		//forward
+								state<=idleFW;
+								nextaddr<=currentaddr;	
+								end
+							else if(B) begin		//start backward
+								state<=BW;
+								nextaddr<=currentaddr+1'b1;
+								end
+							else begin		//keep idle
+								state<=idleBW;
+								nextaddr<=currentaddr;
+								end
 
-			FW:			if(R) begin
-							state<=FW;
-							nextaddr<=1'b0;
-						end
-						else if(D) begin
-							state<=idleFW;
-							nextaddr<=currentaddr;
-						end
-						else begin
-							state<=FW;
-							nextaddr<=currentaddr+1'b1;						
-						end
-			BW:
-					if(R) begin
-						state<=BW;
-						nextaddr<=1'b0;
-						end
-					else if(D) begin
-						state<=idleBW;
-						nextaddr<=currentaddr;
-						end
-					else begin
-						state<=BW;
-						nextaddr<=currentaddr-1'b1;
-						end
-		endcase
+					FW:			if(R) begin		//reset
+									state<=FW;
+									nextaddr<=1'b0;
+								end
+								else if(B) begin		//backward
+									state<=BW;
+									nextaddr<=currentaddr-1'b1;	
+								end
+								else if(D) begin		//idle
+									state<=idleFW;
+									nextaddr<=currentaddr;
+								end
+								else begin		//keep forward
+									state<=FW;
+									nextaddr<=currentaddr+1'b1;						
+								end
+					BW:
+							if(R) begin//reset
+								state<=BW;
+								nextaddr<=1'b0;
+								end
+							else if(F) begin		//forward
+								state<=FW;
+								nextaddr<=currentaddr+1'b1;	
+								end
+							else if(D) begin		//idle
+								state<=idleBW;
+								nextaddr<=currentaddr;
+								end
+							else begin		//keep backward
+								state<=BW;
+								nextaddr<=currentaddr-1'b1;
+								end
+				endcase
+			end
 	end
 endmodule
 
