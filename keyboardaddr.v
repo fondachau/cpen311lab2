dbad8948b29ba88fa25d37fa59bdd76d55a22810
lisali42 <lisali42@hotmail.ca>
2017-10-19 19:49:55 -0700

    Update keyboardaddr.v

--- keyboardaddr.v
+++ keyboardaddr.v
@@ -1,67 +1,83 @@
-module keyboardaddr (currentaddr, state, nextstate, D, E, B, F, R,nextaddr);
+module keyboardaddr (currentaddr,clk, state, D, E, B, F, R,nextaddr);
 	input logic [31:0]  currentaddr;
 	input logic D, E, B, F, R;
-	input logic [1:0] state;
+	output logic [1:0] state;
+	input logic clk;
 	output logic [31:0] nextaddr;
-	output logic [1:0] nextstate;
+	//output logic [1:0] nextstate;
 	
 	parameter idleFW = 2'b00;
 	parameter idleBW = 2'b01;
 	parameter FW = 2'b10;
 	parameter BW = 2'b11;
 	
-	always_ff @ (currentaddr) //going to check which buttons are pressed after mem is accessed and played note?
+	vDFF addrVDFF(clk,nextaddr,currentaddr);
+	always_ff @ (posedge clk)
 		begin
 			case(state)
-			default: nextstate<= FW;
+			default: state<= idleFW;
 			idleFW:	//making it so you can only change FW/BW 
 					if(R) begin
-						nextstate<=idleFW;
+						state<=idleFW;
 						nextaddr<=1'b0;
+						end
 					else if(E) begin
-						nextstate<=FW;
+						state<=FW;
 						nextaddr<=currentaddr+1'b1;
 						end
 					else begin
-						nextstate<=idleFW;
+						state<=idleFW;
 						nextaddr<=currentaddr;
 						end
 			idleBW:
 					if(R) begin
-						nextstate<=idleBW;
+						state<=idleBW;
 						nextaddr<=1'b0;
+						end
 					else if(E) begin
-						nextstate<=BW;
+						state<=BW;
 						nextaddr<=currentaddr+1'b1;
 						end
 					else begin
-						nextstate<=idleBW;
+						state<=idleBW;
 						nextaddr<=currentaddr;
 						end
-			FW:
-					if(R) begin
-						nextstate<=FW;
-						nextaddr<=1'b0;
-					else if(D) begin
-						nextstate<=idleFW;
-						nextaddr<=currentaddr;
+
+			FW:			if(R) begin
+							state<=FW;
+							nextaddr<=1'b0;
 						end
-					else begin
-						nextstate<=FW;
-						nextaddr<=currentaddr+1'b1;						
+						else if(D) begin
+							state<=idleFW;
+							nextaddr<=currentaddr;
+						end
+						else begin
+							state<=FW;
+							nextaddr<=currentaddr+1'b1;						
 						end
 			BW:
 					if(R) begin
-						nextstate<=BW;
+						state<=BW;
 						nextaddr<=1'b0;
+						end
 					else if(D) begin
-						nextstate<=idleBW;
+						state<=idleBW;
 						nextaddr<=currentaddr;
 						end
 					else begin
-						nextstate<=BW;
-						nextaddr<=currentaddr+1'b1;
+						state<=BW;
+						nextaddr<=currentaddr-1'b1;
 						end
 		endcase
 	end
+endmodule
+
+module vDFF(clk, in, out);
+	input logic clk;
+	input logic [31:0] in;
+
+	output logic [31:0] out;
+
+	always_ff @(posedge clk)
+		out<=in;
 endmodule
